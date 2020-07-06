e3b6989ba9d81aa1455cfca7e0ab35fb3c8d6518
vagrant <vagrant@homestead>
2020-07-06 14:41:09 +0000

    weibo list

---
+++ resources/views/statuses/_status.blade.php
@@ -0,0 +1,9 @@
+<li class='meida mt-4 mt-4'>
+    <a href="{{ route('users.show', $user->id) }}">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar">
+    </a>
+    <div class="media-body">
+        <h5 class="mb-0 mb-1">{{ $user->name }}<small> / {{ $status->created_at->diffForHumans() }}</small></h5>
+        {{ $status->content }}
+    </div>
+</li>
